87d2ade1b0e832a535c84a6fb515105101485ea5
Sergio Muiños <sergiomgarc@ext.inditex.com>
2022-04-28 07:37:56 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+
+    apiKey: "AIzaSyCyhMuDIkzFp4ITPG6l16eFpBEiUC1CAaM",
+  
+    authDomain: "empleados-d4167.firebaseapp.com",
+  
+    projectId: "empleados-d4167",
+  
+    storageBucket: "empleados-d4167.appspot.com",
+  
+    messagingSenderId: "856808831066",
+  
+    appId: "1:856808831066:web:d4ace82517b4ee14770063"
+  
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+      apiKey: "AIzaSyCyhMuDIkzFp4ITPG6l16eFpBEiUC1CAaM",
+      authDomain: "empleados-d4167.firebaseapp.com",
+      projectId: "empleados-d4167",
+      storageBucket: "empleados-d4167.appspot.com",
+      messagingSenderId: "856808831066",
+      appId: "1:856808831066:web:d4ace82517b4ee14770063"
+    
+    }
 };
 
 /*
